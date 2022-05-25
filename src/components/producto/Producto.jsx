f07f71bc23ec43e696619b537c6c6e3cdf916faf
Angel <romeroangel025@gmail.com>
2022-05-25 15:42:15 -0300

    test

--- src/components/producto/Producto.jsx
+++ src/components/producto/Producto.jsx
@@ -3,7 +3,7 @@ import './producto.css'
 const Producto = () => {
   return (
     <section className='producto__container'>
-        
+        <h1>hello</h1>
         
         Producto
          {/*  Reemplazar todo el codigo por el de Angel */}
