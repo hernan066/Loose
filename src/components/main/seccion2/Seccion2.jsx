5b7b77af5062a21fbb8f70fd91bd8a451e45821c
hernan066 <hernan0066@gmail.com>
2022-05-26 14:17:35 -0300

    Responsive seccion 2 y 3. Import font Oswald

--- src/components/main/seccion2/Seccion2.jsx
+++ src/components/main/seccion2/Seccion2.jsx
@@ -4,40 +4,45 @@ import images from "../../../assets/images";
 const Seccion2 = () => {
   return (
     <div className="container__slider">
+      <h1>I'm LOOSE</h1>
+
       <div className="container">
         <input type="radio" name="slider" id="item-1" defaultChecked />
         <input type="radio" name="slider" id="item-2" />
         <input type="radio" name="slider" id="item-3" />
         <div className="cards">
-         {/*  <div className="slider__squat"></div> */}
-          
+          {/*  <div className="slider__squat"></div> */}
+
           <label className="card" htmlFor="item-1" id="selector-1">
-           
-              <img
-                src={images.remera1}
-                alt="remera1"
-              />
+            <img src={images.remera1} alt="remera1" />
             <img src={images.logo} alt="logo" id="image-logo" />
           </label>
           <label className="card" htmlFor="item-2" id="selector-2">
-           
-              <img
-               src={images.remera2}
-                alt="remera2"
-              />
-           <img src={images.logo} alt="logo" id="image-logo" />
+            <img src={images.remera2} alt="remera2" />
+            <img src={images.logo} alt="logo" id="image-logo" />
           </label>
 
           <label className="card" htmlFor="item-3" id="selector-3">
-            
-              <img
-               src={images.remera3}
-                alt="remera3"
-              />
-           <img src={images.logo} alt="logo" id="image-logo" />
+            <img src={images.remera3} alt="remera3" />
+            <img src={images.logo} alt="logo" id="image-logo" />
           </label>
+         
         </div>
       </div>
+    
+      <div className="container__img-smart-screen">
+            <div className="container__img-smart-screen-card">
+              <img src={images.remera1} alt="remera1" />
+            </div>
+            <div className="container__img-smart-screen-card">
+              <img src={images.remera2} alt="remera2" />
+            </div>
+            <div className="container__img-smart-screen-card">
+              <img src={images.remera3} alt="remera3" />
+            </div>
+          </div>
+    
+    
     </div>
   );
 };
