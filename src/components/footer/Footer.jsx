bbc09428ac5037a18aeb5d5231d241115525ee4d
Meli Gimenez <melinagimenez1999@gmail.com>
2022-06-11 13:27:13 -0300

    guardando nuevos cambios

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,16 +2,11 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <footer className='footer__container'>
+    <footer className='footer_container'>
         
-        
-    <div className='sobreNosotros'> 
-    <p id='sobreNosotrosTitulo'>SOBRE NOSOTROS</p>  
-    <p>Blablablablablablablballablablablablablablablbalb.</p>  
-    </div>
-     
-      
-
+        <div id='locales' >'HOLA'</div>
+        <div id='sobre_nosotros' >'CHAU</div>
+        <div id='iconos'>'CHAU CHAUU'</div>
 
 
     </footer>
