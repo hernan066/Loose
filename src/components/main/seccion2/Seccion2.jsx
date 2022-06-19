2de2b3e51d0b0d8c5999843be3898aea936d50fc
hernan0066 <hernan0066@gmail.com>
2022-06-18 23:43:02 -0300

    Animation seccion2 mobile whith scroll

--- src/components/main/seccion2/Seccion2.jsx
+++ src/components/main/seccion2/Seccion2.jsx
@@ -1,47 +1,93 @@
 import "./seccion2.css";
 import images from "../../../assets/images";
+import { scrollAnimateUp } from "../../../utils/animationVariats";
+import { motion } from "framer-motion";
 
 const Seccion2 = () => {
   return (
-    <>
-      <h2 className="seccion2__title">I'm LOOSE</h2>
+    <section className="seccion2__container">
+      <motion.h2
+        initial={"initial"}
+        whileInView={"animate"}
+        viewport={{ once: false, amount: 0.2 }}
+        variants={scrollAnimateUp}
+        className="seccion2__title"
+      >
+        I'm LOOSE
+      </motion.h2>
       <div className="container__slider">
-        <div className="container">
+        <motion.div
+          className="section2__slider"
+          initial={"initial"}
+          whileInView={"animate"}
+          viewport={{ once: false, amount: 0.2 }}
+          variants={scrollAnimateUp}
+        >
           <input type="radio" name="slider" id="item-1" defaultChecked />
           <input type="radio" name="slider" id="item-2" />
           <input type="radio" name="slider" id="item-3" />
           <div className="cards">
-            {/*  <div className="slider__squat"></div> */}
-
-            <label className="card" htmlFor="item-1" id="selector-1">
+            <label
+              className="section2__slider_card"
+              htmlFor="item-1"
+              id="selector-1"
+            >
               <img src={images.remera1} alt="remera1" />
               <img src={images.logo} alt="logo" id="image-logo" />
             </label>
-            <label className="card" htmlFor="item-2" id="selector-2">
+            <label
+              className="section2__slider_card"
+              htmlFor="item-2"
+              id="selector-2"
+            >
               <img src={images.remera2} alt="remera2" />
               <img src={images.logo} alt="logo" id="image-logo" />
             </label>
 
-            <label className="card" htmlFor="item-3" id="selector-3">
+            <label
+              className="section2__slider_card"
+              htmlFor="item-3"
+              id="selector-3"
+            >
               <img src={images.remera3} alt="remera3" />
               <img src={images.logo} alt="logo" id="image-logo" />
             </label>
           </div>
-        </div>
-
-        <div className="container__img-smart-screen">
-          <div className="container__img-smart-screen-card">
-            <img src={images.remera1} alt="remera1" />
-          </div>
-          <div className="container__img-smart-screen-card">
-            <img src={images.remera2} alt="remera2" />
-          </div>
-          <div className="container__img-smart-screen-card">
-            <img src={images.remera3} alt="remera3" />
-          </div>
-        </div>
+        </motion.div>
       </div>
-    </>
+
+      {/* ----------Smart Screen---------- */}
+
+      <div className="container__img-smart-screen">
+        <motion.div
+          className="container__img-smart-screen-card"
+          initial={"initial"}
+          whileInView={"animate"}
+          variants={scrollAnimateUp}
+          viewport={{ once: false, amount: 0.2 }}
+        >
+          <img src={images.remera1} alt="remera1" />
+        </motion.div>
+        <motion.div
+          className="container__img-smart-screen-card"
+          initial={"initial"}
+          whileInView={"animate"}
+          variants={scrollAnimateUp}
+          viewport={{ once: false, amount: 0.2 }}
+        >
+          <img src={images.remera2} alt="remera2" />
+        </motion.div>
+        <motion.div
+          className="container__img-smart-screen-card"
+          initial={"initial"}
+          whileInView={"animate"}
+          variants={scrollAnimateUp}
+          viewport={{ once: false, amount: 0.2 }}
+        >
+          <img src={images.remera3} alt="remera3" />
+        </motion.div>
+      </div>
+    </section>
   );
 };
 
--- src/components/main/seccion4/Seccion4.jsx
+++ src/components/main/seccion4/Seccion4.jsx
@@ -1,34 +1,50 @@
 import "./seccion4.css";
 import images from "../../../assets/images";
+import { scrollAnimateUp } from "../../../utils/animationVariats";
+import { motion } from "framer-motion";
 
 const Seccion4 = () => {
   return (
     <section className="seccion4__container">
-      <h2>productos</h2>
-      
+      <motion.h2
+        initial={"initial"}
+        whileInView={"animate"}
+        viewport={{ once: false, amount: 0.2 }}
+        transition={{ staggerChildren: 0.5 }}
+        variants={scrollAnimateUp}
+      >
+        productos
+      </motion.h2>
 
-      <div className="seccion4__card-container">
-        
-        <div className="seccion4__card">
+      <motion.div
+        className="seccion4__card-container"
+        initial={"initial"}
+        whileInView={"animate"}
+        viewport={{ once: false, amount: 0.2 }}
+        transition={{ staggerChildren: 0.5 }}
+      >
+        <motion.div className="seccion4__card" variants={scrollAnimateUp}>
           <div className="seccion4__card-image">
             <img src={images.remeraBlanca} alt="remera blanca" />
           </div>
           <h3>One White</h3>
-        </div>
-        <div className="seccion4__card">
+        </motion.div>
+        <motion.div className="seccion4__card" variants={scrollAnimateUp}>
           <div className="seccion4__card-image">
             <img src={images.remeraNegra} alt="remera negra" />
           </div>
           <h3>One Black</h3>
-        </div>
-        <div className="seccion4__card">
+        </motion.div>
+        <motion.div className="seccion4__card" variants={scrollAnimateUp}>
           <div className="seccion4__card-image">
-            <img src="http://vision.centroculturadigital.mx/media/done/1550616316563-QR-GlosarioCCD-cont.png" alt="logo qr" />
+            <img
+              src="http://vision.centroculturadigital.mx/media/done/1550616316563-QR-GlosarioCCD-cont.png"
+              alt="logo qr"
+            />
           </div>
           <h3>Codigo QR</h3>
-        </div>
-     
-      </div>
+        </motion.div>
+      </motion.div>
     </section>
   );
 };
