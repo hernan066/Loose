a35c470cc0f03057d3cf5672118d23a5dd52d905
hernan0066 <hernan0066@gmail.com>
2022-06-18 22:49:12 -0300

    Animation seccion3 whith scroll

--- src/components/main/seccion3/Seccion3.jsx
+++ src/components/main/seccion3/Seccion3.jsx
@@ -2,9 +2,11 @@ import React, { useRef, useEffect } from "react";
 import "./seccion3.css";
 import images from "../../../assets/images";
 import hoverEffect from "hover-effect";
+import { motion } from "framer-motion";
+
+import { scrollAnimateUp } from "../../../utils/animationVariats";
 
 export const Seccion3 = () => {
-  
   const container = useRef();
 
   useEffect(() => {
@@ -16,39 +18,56 @@ export const Seccion3 = () => {
       image1: images.remera4,
       image2: images.remera3,
       displacementImage:
-      "https://raw.githubusercontent.com/robin-dela/hover-effect/master/example/img/displacement/1.jpg"
+        "https://raw.githubusercontent.com/robin-dela/hover-effect/master/example/img/displacement/1.jpg",
     });
   }, [container]);
-  
-  
+
   return (
     <section className="seccion3__container">
       <div className="seccion3__background"></div>
 
-      <div className="seccion__text">
-        <span style={{ color: "#fff" }}>HOLGADO </span>
-        <span style={{ color: "#fff" }}>SUELTO</span>
-        <span style={{ color: "#ec5928" }}>EN LIBERTAD!</span>
-        <div className="seccion__text-p">
+      <motion.div
+        className="seccion__text"
+        initial={"initial"}
+        whileInView={"animate"}
+        viewport={{ once: false, amount: 0.2 }}
+        transition={{ staggerChildren: 0.5 }}
+      >
+        <motion.span style={{ color: "#fff" }} variants={scrollAnimateUp}>
+          HOLGADO{" "}
+        </motion.span>
+        <motion.span style={{ color: "#fff" }} variants={scrollAnimateUp}>
+          SUELTO
+        </motion.span>
+        <motion.span style={{ color: "#ec5928" }} variants={scrollAnimateUp}>
+          EN LIBERTAD!
+        </motion.span>
+        <motion.div className="seccion__text-p" variants={scrollAnimateUp}>
           <p>
             I'm a paragraph. Click here to add your own text and edit me. It’s
             easy. Just click “Edit Text” or double click me to add your own
             content and make changes to the font.
           </p>
-        </div>
+        </motion.div>
 
-        <button className="btn">Leer mas...</button>
-      </div>
-      <div className="seccion__image">
-        
+        <motion.button className="btn" variants={scrollAnimateUp}>
+          Leer mas...
+        </motion.button>
+      </motion.div>
+      <motion.div
+        className="seccion__image"
+        initial={"initial"}
+        whileInView={"animate"}
+        viewport={{ once: false, amount: 0.2 }}
+        variants={scrollAnimateUp}
+      >
         <div className="seccion__image-container" ref={container}>
-         
-         <div className="seccion__image_square-bg"></div>
-          <div className="seccion__image_square"></div>  
+          <div className="seccion__image_square-bg"></div>
+          <div className="seccion__image_square"></div>
         </div>
 
         <div className="seccion__image_bg"></div>
-      </div>
+      </motion.div>
     </section>
   );
 };
