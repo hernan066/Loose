48b445518c05d2ab7c8f3491e096cf0351388848
hernan0066 <hernan0066@gmail.com>
2022-06-20 13:46:18 -0300

    Install ReduxToolKit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@
 import './App.css'
 import AppRouter from './router/AppRouter'
 
+import { Provider } from 'react-redux';
+import { store, persistor } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react';
+
 function App() {
  
 
   return (
-    <AppRouter />
+    <Provider store={store}>
+     <PersistGate loading={null} persistor={persistor}>
+     <AppRouter />
+      </PersistGate>
+  </Provider>
   )
 }
 
