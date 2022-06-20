e3de20de97ced8c4b11461ce1f5c7ce99f471516
hernan066 <hernan0066@gmail.com>
2022-06-20 12:47:33 -0300

    Add DatosEnvio

--- src/components/checkout/step1/Step1.jsx
+++ src/components/checkout/step1/Step1.jsx
@@ -1,81 +1,131 @@
-import './step1.css'
+import "./step1.css";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { Link, useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import image from '../../../assets/images'
+
+const SignupSchema = Yup.object().shape({
+  nombre: Yup.string().required("Requerido"),
+  apellido: Yup.string().required("Requerido"),
+  provincia: Yup.string().required("Requerido"),
+  ciudad: Yup.string().required("Requerido"),
+  direccion: Yup.string().required("Requerido"),
+  telefono: Yup.number().required("Requerido"),
+});
 
 export const Step1 = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="order__form">
-    <h2>Completa los datos de envio</h2>
+    <div className="login__main">
+      <div className="order__container">
+        <div className="order__form">
+          <h2>Datos de envio</h2>
 
-    <Formik
-      initialValues={{
-        nombre: "",
-        apellido: "",
-        provincia: "",
-        ciudad: "",
-        direccion: "",
-        telefono: "",
-      }}
-      validationSchema={SignupSchema}
-      onSubmit={(values, { resetForm }) => {
-        //const { email, password } = values;
-        //dispatch(starLogin(values));
-        //login( dispatch, {email, password} );
+          <Formik
+            initialValues={{
+              nombre: "",
+              apellido: "",
+              provincia: "",
+              ciudad: "",
+              direccion: "",
+              telefono: "",
+            }}
+            validationSchema={SignupSchema}
+            onSubmit={(values, { resetForm }) => {
+              //const { email, password } = values;
+              //dispatch(starLogin(values));
+              //login( dispatch, {email, password} );
 
-        //resetForm();
-        console.log(values);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <Field type="text" name="nombre" placeholder="Nombre" />
-          <ErrorMessage
-            name="nombre"
-            component="p"
-            className="login__error"
-          />
+              //resetForm();
+              console.log(values);
+            }}
+          >
+            {({ isSubmitting }) => (
+              <Form>
+                <Field type="text" name="nombre" placeholder="Nombre" />
+                <ErrorMessage
+                  name="nombre"
+                  component="p"
+                  className="login__error"
+                />
 
-          <Field type="text" name="apellido" placeholder="Apellido" />
+                <Field type="text" name="apellido" placeholder="Apellido" />
 
-          <ErrorMessage
-            name="apellido"
-            component="p"
-            className="login__error"
-          />
+                <ErrorMessage
+                  name="apellido"
+                  component="p"
+                  className="login__error"
+                />
 
-          <Field type="number" name="telefono" placeholder="Telefono" />
-          <ErrorMessage
-            name="telefono"
-            component="p"
-            className="login__error"
-          />
-          <Field type="text" name="direccion" placeholder="Direccion" />
-          <ErrorMessage
-            name="direccion"
-            component="p"
-            className="login__error"
-          />
+                <Field type="number" name="telefono" placeholder="Telefono" />
+                <ErrorMessage
+                  name="telefono"
+                  component="p"
+                  className="login__error"
+                />
+                <Field type="text" name="direccion" placeholder="Direccion" />
+                <ErrorMessage
+                  name="direccion"
+                  component="p"
+                  className="login__error"
+                />
 
-          <Field type="text" name="provincia" placeholder="Provincia" />
+                <Field type="text" name="provincia" placeholder="Provincia" />
 
-          <ErrorMessage
-            name="provincia"
-            component="p"
-            className="login__error"
-          />
+                <ErrorMessage
+                  name="provincia"
+                  component="p"
+                  className="login__error"
+                />
 
-          <Field type="text" name="ciudad" placeholder="Ciudad" />
+                <Field type="text" name="ciudad" placeholder="Ciudad" />
 
-          <ErrorMessage
-            name="ciudad"
-            component="p"
-            className="login__error"
-          />
+                <ErrorMessage
+                  name="ciudad"
+                  component="p"
+                  className="login__error"
+                />
 
-          <button type="submit" disabled={isSubmitting}>
-            Siguiente
+                <button type="submit" disabled={isSubmitting}>
+                  Siguiente
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+        <div className="order__detalle">
+          <h3>Resumen de compra</h3>
+          {/*------------------ crear componente------------------- */}
+
+          <div className="order__producto">
+            <div className="order__img">
+              <img src={image.remeraNegra} alt="remera" />
+            </div>
+            <div className="order__title">
+              <h4>Remera negra</h4>
+              <div className="order__price">
+                <p>Cant: 1 </p>
+                <p>$2000</p>
+              </div>
+            </div>
+          </div>
+
+          {/*------------------ crear componente------------------- */}
+
+          <div className="order__total">
+            <h4>Total</h4>
+            <h4>total</h4>
+          </div>
+
+          <button
+            className="order__seguir_comprando"
+            onClick={() => navigate(-1)}
+          >
+            <i className="fas fa-arrow-left"></i> Volver al carrito
           </button>
-        </Form>
-      )}
-    </Formik>
-  </div>
-  )
-}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/router/AnimateRouter.jsx
+++ src/router/AnimateRouter.jsx
@@ -3,9 +3,11 @@ import { AnimatePresence } from "framer-motion";
 
 import Home from "../pages/Home";
 import PageProducto from "../pages/PageProducto";
-import { Step1 } from "../components/checkout/step1/Step1";
-import { Step3 } from "../components/checkout/step3/Step3";
-import { Step2 } from "../components/checkout/step2/Step2";
+
+
+import { DatosEnvio } from "../pages/DatosEnvio";
+import { FinalizaCompra } from "../pages/FinalizaCompra";
+import { ConfirmaDatos } from "../pages/ConfirmaDatos";
 
 export const AnimateRouter = () => {
   const location = useLocation();
@@ -15,9 +17,9 @@ export const AnimateRouter = () => {
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Home />} />
         <Route path="/producto" element={<PageProducto />} />
-        <Route path="/checkout/datos-envio" element={<Step1 />} />
-        <Route path="/checkout/confirma-datos-envio" element={<Step2 />} />
-        <Route path="/checkout/finaliza-compra" element={<Step3 />} />
+        <Route path="/checkout/datos-envio" element={<DatosEnvio />} /> 
+        <Route path="/checkout/confirma-datos-envio" element={<ConfirmaDatos />} />
+        <Route path="/checkout/finaliza-compra" element={<FinalizaCompra />} /> 
       </Routes>
     </AnimatePresence>
   );
