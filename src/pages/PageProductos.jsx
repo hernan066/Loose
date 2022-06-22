c0200791a2787954a6a16bf07a361fc0538a60d6
hernan066 <hernan0066@gmail.com>
2022-06-22 06:15:54 -0300

    Add transition PageProductos

--- src/pages/PageProductos.jsx
+++ src/pages/PageProductos.jsx
@@ -1,12 +1,20 @@
 import Footer from "../components/footer/Footer"
 import Navbar from "../components/navbar/Navbar"
 import { Productos } from "../components/producto/productos/Productos"
+import { AnimatePresence } from "framer-motion"
+import { Transition } from "../components/transition/Transition"
 
 
 
 export const PageProductos = () => {
   return (
     <>
+        <AnimatePresence exitBeforeEnter>
+      <Transition />
+
+      </AnimatePresence>
+        
+        
         <Navbar />
        <Productos />
        
