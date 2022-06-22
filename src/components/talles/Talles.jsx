3d782cb5a7a07bbec7fe1fadb7eecdaae43104bc
hernan066 <hernan0066@gmail.com>
2022-06-21 22:47:14 -0300

    Add page Tabla-talles

---
+++ src/components/talles/Talles.jsx
@@ -0,0 +1,10 @@
+import './talles.css'
+
+export const Talles = () => {
+  return (
+    <main className='talles__container'>
+
+
+    </main>
+  )
+}
--- src/router/AnimateRouter.jsx
+++ src/router/AnimateRouter.jsx
@@ -3,13 +3,12 @@ import { AnimatePresence } from "framer-motion";
 
 import Home from "../pages/Home";
 import PageProducto from "../pages/PageProducto";
-
-
 import { DatosEnvio } from "../pages/DatosEnvio";
 import { FinalizaCompra } from "../pages/FinalizaCompra";
 import { ConfirmaDatos } from "../pages/ConfirmaDatos";
 import { PageQr } from "../pages/PageQr";
 import { PageContacto } from "../pages/PageContacto";
+import { TablaTalles } from "../pages/TablaTalles";
 
 export const AnimateRouter = () => {
   const location = useLocation();
@@ -19,11 +18,15 @@ export const AnimateRouter = () => {
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Home />} />
         <Route path="/producto" element={<PageProducto />} />
-        <Route path="/checkout/datos-envio" element={<DatosEnvio />} /> 
-        <Route path="/checkout/confirma-datos-envio" element={<ConfirmaDatos />} />
+        <Route path="/checkout/datos-envio" element={<DatosEnvio />} />
+        <Route
+          path="/checkout/confirma-datos-envio"
+          element={<ConfirmaDatos />}
+        />
         <Route path="/checkout/finaliza-compra" element={<FinalizaCompra />} />
-        <Route path="/qr" element={<PageQr />} /> 
-        <Route path="/contacto" element={<PageContacto/>} /> 
+        <Route path="/qr" element={<PageQr />} />
+        <Route path="/contacto" element={<PageContacto />} />
+        <Route path="/tabla-talles" element={<TablaTalles />} />
       </Routes>
     </AnimatePresence>
   );
