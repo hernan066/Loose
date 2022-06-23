61cd321d999dd09b3fefe5498f7fb80f2d7409d1
Meli Gimenez <melinagimenez1999@gmail.com>
2022-06-23 20:05:03 -0300

    agregando iconos

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,18 +7,26 @@ const Footer = () => {
         <div id='derechos__reservados' > © 2022 Loose, Inc. Todos los derechos reservados. </div>
         <div id='sobre_nosotros' >'CHAU</div>
         <div id='iconos'> 
+          
+          <div className='footer__iconos__container'>
+          <a target='blank' href='http://www.twitter.com'><i className="fa-brands fa-twitter"></i></a>
+          </div>
           <div className='footer__iconos__container'>
           <a target='blank' href='http://www.facebook.com'><i className="fa-brands fa-facebook"></i></a>
           </div>
           <div className='footer__iconos__container'>
-          <a target='blank' href='http://www.twitter.com'><i className="fa-brands fa-twitter"></i></a>
+          <a target='blank' href='http://www.telegram.com'><i className="fa-brands fa-telegram"></i></a>
+          </div>
+          <div className='footer__iconos__container'>
+          <a target='blank' href='http://www.whatsapp.com'><i className="fa-brands fa-whatsapp"></i></a>
           </div>
           <div className='footer__iconos__container'>
           <a target='blank' href='http://www.instagram.com'><i className="fa-brands fa-instagram"></i></a>
           </div>
           <div className='footer__iconos__container'>
-          <a target='blank' href='http://www.telegram.com'><i className="fa-brands fa-telegram"></i></a>
+          <a target='blank' href='http://www.youtube.com'><i className="fa-brands fa-youtube"></i></a>
           </div>
+          
           
         </div>
 
