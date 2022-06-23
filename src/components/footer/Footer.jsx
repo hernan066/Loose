2ca43265a59f5903bb0ebc38558bf9608a652009
Meli Gimenez <melinagimenez1999@gmail.com>
2022-06-23 19:09:16 -0300

    poniendole vida al footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,11 +2,25 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <footer className='footer_container'>
+    <footer className='footer__container'>
         
-        <div id='locales' > © 2022 Loose, Inc. Todos los derechos reservados. </div>
+        <div id='derechos__reservados' > © 2022 Loose, Inc. Todos los derechos reservados. </div>
         <div id='sobre_nosotros' >'CHAU</div>
-        <div id='iconos'>'CHAU CHAUU'</div>
+        <div id='iconos'> 
+          <div className='footer__iconos__container'>
+          <a target='blank' href='http://www.facebook.com'><i className="fa-brands fa-facebook"></i></a>
+          </div>
+          <div className='footer__iconos__container'>
+          <a target='blank' href='http://www.twitter.com'><i className="fa-brands fa-twitter"></i></a>
+          </div>
+          <div className='footer__iconos__container'>
+          <a target='blank' href='http://www.instagram.com'><i className="fa-brands fa-instagram"></i></a>
+          </div>
+          <div className='footer__iconos__container'>
+          <a target='blank' href='http://www.telegram.com'><i className="fa-brands fa-telegram"></i></a>
+          </div>
+          
+        </div>
 
 
     </footer>
