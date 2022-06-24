4d34a374896d4739139c767bdc073f1a8b194216
EzeD03 <ezedrojas03@gmail.com>
2022-06-24 16:09:58 -0300

    banner_mobile version

--- src/components/contacto/Contacto.jsx
+++ src/components/contacto/Contacto.jsx
@@ -6,7 +6,7 @@ export const Contacto = () => {
         <section className="contact__container">
             <div className="sct1__container">
                 <div className="sct1_textcontain">
-                    <h1 className="sct1__text">CONTACTO</h1>
+                    <p /* className="sct1__text" */>CONTACTO</p>
                 </div>
                 <div className="sct1__imagecontainer">
                     <img className="sct1__image" src={images.contacto_banner} alt="imagenfacha" />
@@ -14,11 +14,12 @@ export const Contacto = () => {
             </div>
             <div className="sct2__container">
                 <div className="sct2_cajanegra"></div>
-                <h2>holapa</h2>
+                <div className="sct2_imagebloq"></div>
                 
-                
+
+
 
             </div>
         </section>
-    ) 
+    )
 } 
