182cc84cbeb58039410d341d041abc70d4974142
Meli Gimenez <melinagimenez1999@gmail.com>
2022-06-24 13:15:57 -0300

    agregando y dividiendo las partes del footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,43 +1,75 @@
-import './footer.css'
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <footer className='footer__container'>
-      <div id='derechos__reservados' > © 2022 Loose, Inc. Todos los derechos reservados. </div>
-        <div id='sobre_nosotros' >'CHAU</div>
-          <div id='iconos'> 
+    <footer className="footer__container">
+
+      <div className="footer__container__links">
+        <div className="footer__container__links__izquierda">
+        <p>Chauuuuuuua</p>
+          <p>Chaisjdlakjdklad</p>
+          <p>oasidjasjdlakjdlkajdklajdlkaj</p>
+          <p>Chacuuuuuuu</p>
+        </div>
+        <div className="footer__container__links__centro">
+          Chauuuuuuua
           
-            <div className='footer__iconos__container'>
-            <a target='blank' href='http://www.twitter.com'><i className="fa-brands fa-twitter"></i></a>
-            </div>
-
-            <div className='footer__iconos__container'>
-            <a target='blank' href='http://www.facebook.com'><i className="fa-brands fa-facebook-f"></i></a>
-            </div>
-
-            <div className='footer__iconos__container'>
-            <a target='blank' href='http://www.telegram.com'><i className="fa-brands fa-telegram"></i></a>
-            </div>
-
-            <div className='footer__iconos__container'>
-            <a target='blank' href='http://www.whatsapp.com'><i className="fa-brands fa-whatsapp"></i></a>
-            </div>
-
-            <div className='footer__iconos__container'>
-            <a target='blank' href='http://www.instagram.com'><i className="fa-brands fa-instagram"></i></a>
-            </div>
-
-            <div className='footer__iconos__container'>
-            <a target='blank' href='http://www.youtube.com'><i className="fa-brands fa-youtube"></i></a>
-            </div>
+        </div>
+        <div className="footer__container__links__derecha">
+          <div className="footer__container__links__derecha__full__arriba">
+          <div className="footer__container__links__derecha__arriba__iconos">
+            <a target="blank" href="http://www.twitter.com">
+              <i className="fa-brands fa-twitter"></i>
+            </a>
           </div>
-          
-          
 
-         
+          <div className="footer__container__links__derecha__arriba__iconos">
+            <a target="blank" href="http://www.facebook.com">
+              <i className="fa-brands fa-facebook-f"></i>
+            </a>
+          </div>
 
+          <div className="footer__container__links__derecha__arriba__iconos">
+            <a target="blank" href="http://www.telegram.com">
+              <i className="fa-brands fa-telegram"></i>
+            </a>
+          </div>
+
+          <div className="footer__container__links__derecha__arriba__iconos">
+            <a target="blank" href="http://www.tiktok.com">
+              <i className="fa-brands fa-tiktok"></i>
+            </a>
+          </div>
+
+          <div className="footer__container__links__derecha__arriba__iconos">
+            <a target="blank" href="http://www.whatsapp.com">
+              <i className="fa-brands fa-whatsapp"></i>
+            </a>
+          </div>
+
+          <div className="footer__container__links__derecha__arriba__iconos">
+            <a target="blank" href="http://www.instagram.com">
+              <i className="fa-brands fa-instagram"></i>
+            </a>
+          </div>
+
+          <div className="footer__container__links__derecha__arriba__iconos">
+            <a target="blank" href="http://www.youtube.com">
+              <i className="fa-brands fa-youtube"></i>
+            </a>
+          </div>
+          </div>
+          <div className="footer__container__links__derecha__full__abajo">Hola</div>
+        </div>
+       
+      </div>
+
+      <div className="footer__container__derechos__reservados">
+        {" "}
+        <span>© 2022 Loose, Inc. Todos los derechos reservados.</span>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
